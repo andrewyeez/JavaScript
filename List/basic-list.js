2b4f018e23192b6b547dde9e235155235de9b99c
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-05-21 12:25:00 -0700

    improve notes

--- List/basic-list.js
+++ List/basic-list.js
@@ -20,35 +20,81 @@ function BasicList() {
   // this.remove = remove;
   // this.removeFrom = removeFrom;
 
-  // Inserts the value at index value of size
-  // After executing the size is incremented by 1
+  /**
+   * [Insert]
+   *
+   * Inserts a value at the location of SIZE
+   * SIZE as the index should give back the value
+   * at the end of the array. So we are inserting at
+   * the end of the array.
+   *
+   * @param  {[*]} value [Value to be inserted]
+   * @return {[boolean]} [TRUE]
+   */
   this.insert = function(value) {
     this.data[this.size++] = value;
     return true;
   };
 
-  // Inserts after a specific value
-  // that already exist in the list
-  this.insertAfter = function(value, locationVal){
-    var locationPos = this.find(locationVal);
-    if (locationPos){
-      this.data.splice[locationPos+1,0,value];
-      this.size++;
+  /**
+   * [Remove]
+   *
+   * Looks for the value to be removed
+   * IF found, THEN we use SPLICE to remove the value.
+   *
+   * DECEREMENTS the value of SIZE
+   *
+   * @param  {[*]} value [value to be removed]
+   * @return {[boolean]} [TRUE if removed, FALSE if not found]
+   */
+  this.remove = function(value){
+    var location = this.find(value);
+    if (location){
+      this.data.splice(location,1);
+      --this.size;
       return true;
     }
-    // index given is out of bounds.
+    return false;
+  }
+
+  /**
+   * [Insert After]
+   *
+   * Looks for the location that we want to insert our value AFTER,
+   * IF found, THEN we use SPLICE to insert the value after that
+   * said location.
+   *
+   * INCREMENTS the value of SIZE
+   *
+   * @param  {[*]} value       [Value to be inserted]
+   * @param  {[*]} locationVal [Value that will determine the
+   *                            location of insertion]
+   * @return {[boolean]}       [TRUE if inserted, FALSE if not found]
+   */
+  this.insertAfter = function(value, locationVal){
+    var location = this.find(locationVal);
+    if (location){
+      this.data.splice[location+1,0,value];
+      ++this.size;
+      return true;
+    }
     return false;
   };
 
-  // The find method makes use of the array data structure
-  // that we build upon when creating a list.
+  /**
+   * [Find]
+   *
+   * Looks for a value by using a FOR loop on an array
+   * IF value is found, THEN we return the index value
+   * at the point of location.
+   *
+   * @param  {[*]} value [Value to be found]
+   * @return {[*]}       [FALSE if not found, NUMBER index of found position]
+   */
   this.find = function(value){
     for(var i=0; i < this.data.length; ++i){
-      if(this.data[i] == value){
-        return i;
-      }
+      if(this.data[i] == value){ return i; }
     }
-    // did not find the value
     return false;
   }
 

6e657900a807505b5335f50943ae9cd1b47b3b0a
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-06-05 16:38:07 -0700

    each letter in a string should be surrounded by the symbol '+' return false otherwise

---
+++ AlgorithmChallenge/Easy/SimpleSymbols.js
@@ -0,0 +1,23 @@
+// the function SimpleSymbols(str) take the str parameter being
+// passed and determine if it is an acceptable sequence by either
+// returning the string true or false. The str parameter will be
+// composed of + and = symbols with several letters between them
+// (ie. ++d+===+c++==a) and for the string to be true each letter
+// must be surrounded by a + symbol. So the string to the left
+// would be false. The string will not be empty and will have at
+// least one letter.
+
+function SimpleSymbols(str){
+  for(var i=0; i<str.length; i++){
+    if(str[i].match(/^[A-z]+$/)){
+      var back = i-1;
+      var front = i+1;
+      var condition = (str[back]=='+' && str[front]=='+');
+      if(!condition){ return false; }
+    }
+  }
+  return true;
+}
+
+var str = '+l+3+g+';
+console.log(SimpleSymbols(str));
